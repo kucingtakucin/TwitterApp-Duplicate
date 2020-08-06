79a8850d5ce29e746cf7e21cbfc7ca07cda88962
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-06 21:30:10 +0700

    Menambahkan sedikit interaktifitas di activity nya

--- app/src/main/java/com/android/twitterapp/MainActivity.kt
+++ app/src/main/java/com/android/twitterapp/MainActivity.kt
@@ -2,11 +2,27 @@ package com.android.twitterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.android.twitterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+        dataBinding.apply {
+            buttonFollow.setOnClickListener{
+                Toast.makeText(this@MainActivity, "Hore kamu difollow!", Toast.LENGTH_SHORT).show()
+            }
+            website.setOnClickListener{
+                Toast.makeText(this@MainActivity, "Kunjungi website kita ya!",Toast.LENGTH_SHORT).show()
+            }
+            joined.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Ayo join gais", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
